0f06e89a167bdcb9181824efe791c4888f512b8b
Aaron W. Hsu <arcfide@sacrideo.us>
2016-11-04 19:20:22 -0400

    Make the presentation nicer and add timing

--- 2017-spring-pldi/benchmark.c
+++ 2017-spring-pldi/benchmark.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
-typedef int I;typedef void V;typedef size_t B;typedef char C;
-typedef struct cn{B i;I t;B c;struct cn*k[];}*CN;
+#include <time.h>
+typedef int I;typedef void V;typedef size_t B;typedef char C;typedef double D;
+typedef struct cn{B i;I t;B c;struct cn*k[];}*CN;typedef clock_t CT;
 #define R return
 #define P printf
+#define EQ(s1,s2) (!strcmp((s1),(s2)))
 #define DO(n,x) for(I i=0;i<(n);i++){x;}
+#define TIME(x) {CT s=clock();x;CT e=clock();\
+ P("Took %0.3f secs.\n",(D)(e-s)/CLOCKS_PER_SEC);}
 B id=0;
 V pi(I i){P("%d",i);}V nl(V){P("\n");}
 V pp(B d,B w,C*s){P("Benchmarking %s algorithm (Depth: %d, Width: %d)...\n",s,d,w);}
-V help(V){P("benchmark <cpu|gpu> [<depth> <width>]\n");exit(1);}
+V help(V){P("benchmark <cpu|gpu> <print|quiet> [<depth> <width>]\n");exit(1);}
 V merr(V){P("Out of Memory.\n");exit(2);}
 
 CN mcn(I i,I t,B c){CN n=malloc(sizeof(struct cn)+c*sizeof(CN));if(!n)merr();
@@ -23,12 +27,13 @@ B fltc2(CN r,CN t,B c){
   DO(t->c,{CN k=t->k[i];t->k[i]=k->t?k:mcn(k->i,1,0);c=fltc2(r,k,c);})}
  R c;}
 CN fltc(CN t){CN r=mcn(id++,0,fltc1(t));fltc2(r,t,0);R r;}
-V cpu(B d,B w){pp(d,w,"CPU");P("Before\n");CN t=mct(d,w);pct(t,1);
- P("After\n");t=fltc(t);pct(t,1);fct(t);}
+V cpu(B d,B w,I p){pp(d,w,"CPU");CN t=mct(d,w);if(p){P("Before\n");pct(t,1);}
+ TIME(t=fltc(t));if(p){P("After\n");pct(t,1);}fct(t);}
 
-V gpu(B d,B w){pp(d,w,"GPU");}
+V gpu(B d,B w,I p){pp(d,w,"GPU");}
 
-I main(I c,C*v[]){B d=3,w=2;if(c!=2&&c!=4)help();
- if(c==4){d=atoi(v[2]);w=atoi(v[3]);}
- if(!strcmp(v[1],"cpu"))cpu(d,w);else if(!strcmp(v[1],"gpu"))gpu(d,w);else help();
+I main(I c,C*v[]){B d=3,w=2;I p=1;if(c!=3&&c!=5)help();
+ if(c==5){d=atoi(v[3]);w=atoi(v[4]);}
+ if(EQ(v[2],"print"))p=1;else if(EQ(v[2],"quiet"))p=0;else help();
+ if(EQ(v[1],"cpu"))cpu(d,w,p);else if(EQ(v[1],"gpu"))gpu(d,w,p);else help();
  R 0;}
