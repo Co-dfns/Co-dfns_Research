90aae5640e61a9b34f9215688ff5d5004879e177
Rodrigo Girão Serrão <5621605+RodrigoGiraoSerrao@users.noreply.github.com>
2022-03-03 01:10:29 +0000

    Add new figure version.

--- 2022-icfp-unet/Paper/benchmark-fig-generation.R
+++ 2022-icfp-unet/Paper/benchmark-fig-generation.R
@@ -3,7 +3,7 @@ library(ggplot2)
 
 # Terrible R code but gets the job done.
 
-data.file <- "./Raw Performance Numbers.txt.txt"
+data.file <- "./Raw Performance Numbers.txt"
 data <- as.matrix(read.csv(data.file, header = FALSE, sep = " "))
 
 times <- c()
@@ -17,11 +17,19 @@ for (i in (1:nrow(data))) {
 
 df <- data.frame(time = as.numeric(times), source = sources)
 
-df %>% ggplot(aes(x = reorder(source, time), y = time)) +
-    geom_boxplot() +
+orig_sources <- c("Dyalog", "Co-dfns(CPU)", "PyTorch(CPU)", "Co-dfns(GPU)", "PyTorch(SMP)", "PyTorch(GPU)")
+colours <- c("#fc9732", "#4787e6", "#ff002b", "#284b80", "#cc0023", "#800016")
+df %>% group_by(source) %>%
+    summarise(meant = mean(time), min = min(time), max = max(time)) %>%
+    ggplot(aes(x = reorder(source, meant), y = meant, min = min, max = max, nudge = max - min)) +
+    geom_col(aes(fill = source)) +
+    geom_pointrange(aes(ymin = min, ymax = max)) +
+    geom_errorbar(aes(ymin = min, ymax = max), width = .3) +
+    geom_text(aes(label = round(meant, 2)), nudge_y = 5) +
     coord_flip() +
     ggtitle("Execution time of a forward and backward pass in several computational models.") +
     xlab("Computation model") +
     ylab("Execution time (s)") +
-    scale_y_continuous(trans = "log2")
+    scale_fill_manual(values = colours, limits = orig_sources)
+    # scale_y_continuous(trans = "log2", breaks = c(2, 4, 8, 16, 32, 64))
     # stat_summary(geom="text", fun.y = median, aes(label = sprintf("%2.2f", 2^..y..)), position=position_nudge(y=.2))
